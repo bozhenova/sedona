6b4a11e2cd9290c444159c497f21995912516ded
Margo <margo.bozhenova@gmail.com>
2020-05-22 02:04:41 +0300

    Fix gulp.js and add IIFE

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
-let gulp = require('gulp'),
-  browserSync = require('browser-sync'),
-  autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp"),
+  browserSync = require("browser-sync"),
+  autoprefixer = require("gulp-autoprefixer"),
+  rename = require("gulp-rename"),
+  minify = require("gulp-csso");
 
-gulp.task('css', function() {
+gulp.task("css", function () {
   return gulp
-    .src('./*.css')
-    .pipe(autoprefixer(['last 4 versions'], { cascade: true }))
-    .pipe(gulp.dest('./css'))
+    .src("./*.css")
+    .pipe(autoprefixer(["last 4 versions"], { cascade: true }))
+    .pipe(gulp.dest("./css"))
+    .pipe(minify())
+    .pipe(gulp.dest("./css"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./css"))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('html', function() {
-  return gulp.src('./*.html').pipe(browserSync.reload({ stream: true }));
+gulp.task("html", function () {
+  return gulp.src("./*.html").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('js', function() {
-  return gulp.src('./*.js').pipe(browserSync.reload({ stream: true }));
+gulp.task("js", function () {
+  return gulp.src("./*.js").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function () {
   browserSync.init({
-    server: { baseDir: './' }
+    server: { baseDir: "./" },
   });
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./*.css', gulp.parallel('css'));
-  gulp.watch('./*.html', gulp.parallel('html'));
-  gulp.watch('./*.js', gulp.parallel('js'));
+gulp.task("watch", function () {
+  gulp.watch("./*.css", gulp.parallel("css"));
+  gulp.watch("./*.html", gulp.parallel("html"));
+  gulp.watch("./*.js", gulp.parallel("js"));
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
